440f670a2ed2030a1fbf9454372a6e430327855c
TonkaNut <hynnxi@gnu.ac.kr>
2023-11-05 02:43:32 +0900

    cmtNovTrd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Header from './pages/Header'
+//import Header from './pages/Header'
 import Home from './pages/Home'
 import Celebirity from './pages/Celebirity'
 import TV from './pages/TV'
